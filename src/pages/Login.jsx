96e6adf4194b8b4e20090f7ce81c73487b4ced61
manthanvaghasiya <manthanvaghasiya60@gmail.com>
2025-11-27 13:47:40 +0530

    Connected to live backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', formData);
+      const res = await axios.post('https://wealthfolio-api.onrender.com/api/auth/login', formData);
       
       // Save token
       localStorage.setItem('token', res.data.token);
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -12,7 +12,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       // Send data to backend
-      const res = await axios.post('http://localhost:5000/api/auth/register', formData);
+      const res = await axios.post('https://wealthfolio-api.onrender.com/api/auth/register', formData);
       
       // Save the token to local storage (The Browser's safe)
       localStorage.setItem('token', res.data.token);
