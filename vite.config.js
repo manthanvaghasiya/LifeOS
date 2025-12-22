fdd12a4a536e9bea449193c00d72c6dd4d145b8f
manthanvaghasiya <manthanvaghasiya60@gmail.com>
2025-12-22 14:03:08 +0530

    Final commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 1. Increase the warning limit to 1000kb (1MB) to silence the warning
+    chunkSizeWarningLimit: 1000, 
+    
+    // 2. Tell Vite to split vendor libraries (node_modules) into a separate file
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
 })
