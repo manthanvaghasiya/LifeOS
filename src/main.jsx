cd08466d3f31e0f4adf842fe9d607be53575f54f
Manthan vaghasiya <manthanvaghasiya60@gmail.com>
2025-12-28 09:32:44 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ThemeProvider } from './context/ThemeContext'; // <--- Import
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider> {/* <--- Wrap App */}
-      <App />
-    </ThemeProvider>
+
+    <App />
+
   </React.StrictMode>,
 )
